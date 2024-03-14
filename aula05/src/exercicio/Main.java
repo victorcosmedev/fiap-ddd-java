58574218d560e8d8cf3f834724eecfd41f5c504e
victorcosmedev <victorcosme.profissional@gmail.com>
2024-03-14 20:04:13 -0300

    exercicio 5_3 corrigido

--- aula05/src/exercicio/Main.java
+++ aula05/src/exercicio/Main.java
@@ -1,12 +1,18 @@
 package exercicio;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String args[]) {
 		ItemPedido livroUm = new ItemPedido("1984", 60.00);
 		ItemPedido livroDois = new ItemPedido("Grokking Algorithms", 250.00);
 		ItemPedido livroTres = new ItemPedido("Percy Jackson e o Ladrão de Raios", 100.00);
 		
-		Pedido pedidoUm = new Pedido(livroUm, livroDois, livroTres);
+		ArrayList<ItemPedido> itens = new ArrayList<>();
+		itens.add(item1);
+		itens.add(item2);
+		
+		Pedido pedidoUm = new Pedido(itens);
 		
 		System.out.println("O valor total do pedido é de: " + pedidoUm.somaDosItens);
 	}
