5847d7c8eb0226fb3be54deaaa7879725971650c
victorcosmedev <victorcosme.profissional@gmail.com>
2024-04-08 22:38:50 -0300

    checkpoint exercicio flecha beneficios

--- aula09/LivrariaFiap/src/pacote/Roteiro.java
+++ aula09/LivrariaFiap/src/pacote/Roteiro.java
@@ -6,23 +6,14 @@ public class Roteiro {
 	private String conteudo;
 	
 	public Roteiro(String disciplina, String nomeAula, String conteudo) {
+
 		this.disciplina = disciplina;
 		this.nomeAula = nomeAula;
 		this.conteudo = conteudo;
 	}
-	
-	String getDisciplina() {
-		return disciplina;
-	}
-	
-	String getNomeAula() {
-		return nomeAula;
-	}
-	
-	String getConteudo() {
-		return conteudo;
-	}
-	
+
+
+
 	void exibirRoteiro() {
-		System.out.println("- Disciplina: " + this.getDisciplina() + ". Aula: " + this.getNomeAula() + ". Conteúdo: " + this.getConteudo());	}
+		System.out.println("- Disciplina: " + this.disciplina + ". Aula: " + this.nomeAula + ". Conteúdo: " + this.conteudo);	}
 	}
