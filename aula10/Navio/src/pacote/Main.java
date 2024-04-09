3c1b0f327b7463e2141428d2d1bdb5f6c6f28879
victorcosmedev <victorcosme.profissional@gmail.com>
2024-04-08 22:41:03 -0300

    backup classes

--- aula10/Navio/src/pacote/Main.java
+++ aula10/Navio/src/pacote/Main.java
@@ -1,19 +1,10 @@
 package pacote;
 
-import teste.Navio;
-import teste.Comandante;
-
 public class Main {
-	public static void main(String[] args) {
-		Navio navioUm = new Navio("Belize", 85);
-		Navio navioDois = new Navio("Titanic", 99);
-		
-		Comandante comandante1 = new Comandante("Cleverson", navioUm);
-		Comandante comandante2 = new Comandante("Olhos", navioDois);
-		
-		navioUm.aumentarVelocidade(16);
-		navioDois.aumentarVelocidade(1);
-		
-		
-	}
+	Comandante comandante1 = new Comandante("Cleverson");
+	Comandante comandante2 = new Comandante("Olhos");
+	
+	Navio navioUm = new Navio("Belize", 85);
+	Navio navioDois = new Navio("Titanic", 99);
+	
 }
